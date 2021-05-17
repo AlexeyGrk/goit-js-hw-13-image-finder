53bef2a1a3344a6c7cb232dd072c3dab8c7c3ad8
AlexeyGrk <alexgmirko@gmail.com>
2021-05-17 19:11:53 +0300

    refactor on class model and draft ;2Dn2

--- src/apiService.js
+++ src/apiService.js
@@ -1,13 +1,34 @@
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '21657672-6f26057767faea3bb550eec99';
 
-export default async function fetchPictureByWord(searchQuery) {
-  try {
-    const response =
-      await fetch(`${BASE_URL}/?image_type=photo&orientation=horizontal&q=${searchQuery}&page=1&per_page=12&key=${API_KEY}
+export default class PictureApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
+  async fetchPictureByWord() {
+    try {
+      const response =
+        await fetch(`${BASE_URL}/?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${API_KEY}
 `);
-    const data = await response.json();
-    const dataList = await data.hits;
-    return dataList;
-  } catch {}
+      const data = await response.json();
+      const dataList = await data.hits;
+      this.page += 1;
+      console.log(dataList);
+      return dataList;
+    } catch {}
+  }
+  get query() {
+    return this.searchQuery;
+  }
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
 }
+// function fetchPictureByWord() {
+//   fetch(
+//     `${BASE_URL}/?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${API_KEY}`,
+//   )
+//     .then(r => r.json())
+//     .then(data => data.hits);
+// }
--- src/index.js
+++ src/index.js
@@ -3,22 +3,36 @@ import fetchPictureByWord from './apiService.js';
 import pictureLists from './templates/pictureLists.hbs';
 import './pnotify-cfg';
 import { error } from '@pnotify/core';
+import PictureApiService from './apiService.js';
 
 const refs = {
   gallery: document.querySelector('.gallery'),
   searchForm: document.querySelector('.search-form'),
   findInput: document.querySelector('.input-picture-js'),
+  findBtn: document.querySelector('.find-btn'),
   loadMoreBtn: document.querySelector('.load-more-btn'),
 };
 
-refs.loadMoreBtn.addEventListener('click', searchPictures);
+const PirctureApiService = new PictureApiService();
+
+refs.findBtn.addEventListener('click', searchPictures);
+refs.loadMoreBtn.addEventListener('click', OnLoadMore);
 
 function searchPictures(e) {
   e.preventDefault();
-  const inputValue = refs.findInput.value;
-  fetchPictureByWord(inputValue).then(renderPicturesGallery);
+  PirctureApiService.query = refs.findInput.value;
+
+  PirctureApiService.fetchPictureByWord().then(renderPicturesGallery);
 }
 function renderPicturesGallery(pictures) {
   const markup = pictureLists(pictures);
   refs.gallery.insertAdjacentHTML('beforeend', markup);
+}
+function OnLoadMore() {
+  PirctureApiService.fetchPictureByWord().then(renderPicturesGallery);
+  window.scrollTo({
+    top: 1500,
+    left: 100,
+    behavior: 'smooth',
+  });
 }
